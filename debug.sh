0d35730685f6ad658615bd456538c01fb5a1bdb5
DoroWu <fcwu.tw@gmail.com>
2015-10-08 10:07:52 +0800

    More info

--- debug.sh
+++ debug.sh
@@ -3,10 +3,19 @@
 ps aux > /tmp/ps.txt
 ls -al /share/CACHEDEV?_DATA/.qpkg/container-station/* > /tmp/ls.txt
 
+supervisorctl -c /share/CACHEDEV?_DATA/.qpkg/container-station/etc/supervisord.conf status > /tmp/supervisor-status.log
+ls -R /share/CACHEDEV?_DATA/.qpkg/container-station/etc/ > /tmp/ls-etc.txt
+ls -l /share/CACHEDEV?_DATA/.qpkg/container-station/var/* > /tmp/ls-var.txt
+
+docker ps -a > /tmp/docker-ps.txt
+docker images > /tmp/docker-image.txt
+
 tar cjvf container-station-log.tbz \
     /etc/platform.conf \
     /etc/default_config/uLinux.conf \
     /etc/config/uLinux.conf \
     /etc/config/qpkg.conf \
     /tmp/{ps,ls}.txt \
+    /tmp/ls-*.txt \
+    /tmp/docker-*.txt \
     /var/log/container-station/*
